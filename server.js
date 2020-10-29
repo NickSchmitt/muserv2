0ae5279c357fb825e16322288a4a0e1d851a6b65
nickschmitt <nickschmitt@gmail.com>
2020-10-29 11:24:43 -0700

    pre pull commit

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.use(express.static(__dirname + '/public'))
 app.use(layouts)
 
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-    })
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
 )
 app.use(flash())
 
@@ -30,10 +30,10 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use((req, res, next) => {
-    // before every route, attach the flash messages and current user to res.locals
-    // res.locals.alerts = req.flash()
-    res.locals.currentUser = req.user
-    next()
+  // before every route, attach the flash messages and current user to res.locals
+  // res.locals.alerts = req.flash()
+  res.locals.currentUser = req.user
+  next()
 })
 
 // app.get('/', (req, res) => {
@@ -43,18 +43,17 @@ app.use((req, res, next) => {
 // })
 
 app.get('/', isLoggedIn, (req, res) => {
-    {
-        res.render('index')
-    }
+  {
+    res.render('index')
+  }
 })
 
-
 app.get('/profile', isLoggedIn, (req, res) => {
-    db.comment.findAll().then((allComments) => {
-        res.render('profile', {
-            comments: allComments,
-        })
+  db.comment.findAll().then((allComments) => {
+    res.render('profile', {
+      comments: allComments,
     })
+  })
 })
 
 // app.get('/profile', isLoggedIn, (req, res) => {
@@ -178,31 +177,26 @@ app.post('/track', (req, res) => {
           // console.log(comment.text)
           res.redirect('back')
         })
+    })
 })
 
-
-app.get('/about', isLoggedIn, function(req, res) {
-    res.render('about')
+app.get('/about', isLoggedIn, function (req, res) {
+  res.render('about')
 })
 
-
-
-app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
-});
-
+app.get('/logout', function (req, res) {
+  req.logout()
+  res.redirect('/')
+})
 
 app.use('/auth', require('./routes/auth'))
 
 var server = app.listen(process.env.PORT || 3000, () =>
-    console.log(
-        `🎧You're listening to the smooth sounds of port ${
+  console.log(
+    `🎧You're listening to the smooth sounds of port ${
       process.env.PORT || 3000
     }🎧`
-    )
+  )
 )
 
 module.exports = server
-
-// This is a comment for a test, and this right here is the second commment to test if the process works
